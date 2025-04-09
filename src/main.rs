62a6669e471910eb4e491407692848c185d24998
code666zzz <1967246624@qq.com>
2025-04-09 16:23:25 +0800

    4/9 update

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 #![no_std]
 #![no_main]
+mod vga_buffer;
 use core::panic::PanicInfo;
 
 #[panic_handler]
 fn panic(_info:&PanicInfo)->!{
 	loop{}
 }
+
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> !{
+	vga_buffer::printf_something();
 	loop{}
 }
 
